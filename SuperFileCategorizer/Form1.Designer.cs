d758dbd53c1882add4fff63c40d6bef1a0704aaa
kdel2050 <163053456+kdel2050@users.noreply.github.com>
2024-03-11 22:30:30 +0200

    Add files via upload

---
+++ SuperFileCategorizer/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SuperFileCategorizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmdSelectInput = new System.Windows.Forms.Button();
+            this.cmdSelectOutput = new System.Windows.Forms.Button();
+            this.folderBInput = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBOutput = new System.Windows.Forms.FolderBrowserDialog();
+            this.cmdCopy = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdSelectInput
+            // 
+            this.cmdSelectInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.cmdSelectInput.ForeColor = System.Drawing.Color.Navy;
+            this.cmdSelectInput.Location = new System.Drawing.Point(57, 38);
+            this.cmdSelectInput.Name = "cmdSelectInput";
+            this.cmdSelectInput.Size = new System.Drawing.Size(215, 48);
+            this.cmdSelectInput.TabIndex = 0;
+            this.cmdSelectInput.Text = "Select &Input Directory";
+            this.cmdSelectInput.UseVisualStyleBackColor = true;
+            this.cmdSelectInput.Click += new System.EventHandler(this.cmdSelectInput_Click);
+            // 
+            // cmdSelectOutput
+            // 
+            this.cmdSelectOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.cmdSelectOutput.ForeColor = System.Drawing.Color.Navy;
+            this.cmdSelectOutput.Location = new System.Drawing.Point(57, 147);
+            this.cmdSelectOutput.Name = "cmdSelectOutput";
+            this.cmdSelectOutput.Size = new System.Drawing.Size(215, 48);
+            this.cmdSelectOutput.TabIndex = 1;
+            this.cmdSelectOutput.Text = "Select &Output Directory";
+            this.cmdSelectOutput.UseVisualStyleBackColor = true;
+            this.cmdSelectOutput.Click += new System.EventHandler(this.cmdSelectOutput_Click);
+            // 
+            // cmdCopy
+            // 
+            this.cmdCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.cmdCopy.ForeColor = System.Drawing.Color.Navy;
+            this.cmdCopy.Location = new System.Drawing.Point(57, 257);
+            this.cmdCopy.Name = "cmdCopy";
+            this.cmdCopy.Size = new System.Drawing.Size(215, 48);
+            this.cmdCopy.TabIndex = 2;
+            this.cmdCopy.Text = "&Copy and Categorize";
+            this.cmdCopy.UseVisualStyleBackColor = true;
+            this.cmdCopy.Click += new System.EventHandler(this.cmdCopy_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(57, 365);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(715, 24);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cmdCopy);
+            this.Controls.Add(this.cmdSelectOutput);
+            this.Controls.Add(this.cmdSelectInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Super File Categorizer 2024 by Kostas A. Delimaris";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdSelectInput;
+        private System.Windows.Forms.Button cmdSelectOutput;
+        private System.Windows.Forms.FolderBrowserDialog folderBInput;
+        private System.Windows.Forms.FolderBrowserDialog folderBOutput;
+        private System.Windows.Forms.Button cmdCopy;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SuperFileCategorizer/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Security.Cryptography;
+
+
+namespace SuperFileCategorizer
+{
+    
+
+    public partial class Form1 : Form
+    {
+        string str_input = "";
+        string str_output = "";
+        string file_dest = "";
+        int counter = 0;
+
+        private static readonly Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+        }
+
+                
+        public static string RandomString(int size, bool lowerCase = false)
+        {
+
+
+            var builder = new StringBuilder(size);
+
+            // Unicode/ASCII Letters are divided into two blocks
+            // (Letters 65–90 / 97–122):
+            // The first group containing the uppercase letters and
+            // the second group containing the lowercase.
+
+            // char is a single Unicode character
+            char offset = lowerCase ? 'a' : 'A';
+            const int lettersOffset = 26; // A...Z or a..z: length=26
+
+            for (var i = 0; i < size; i++)
+            {
+                var @char = (char)_random.Next(offset, offset + lettersOffset);
+                builder.Append(@char);
+            }
+            Application.DoEvents();
+            return lowerCase ? builder.ToString().ToLower() : builder.ToString();
+        }
+
+
+        private void cmdSelectInput_Click(object sender, EventArgs e)
+        {
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+
+            DialogResult result = folderBInput.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBInput.SelectedPath))
+            {
+                str_input = folderBInput.SelectedPath;
+                MessageBox.Show(str_input + " selected as input directory");
+
+            }
+        }
+
+        private void cmdSelectOutput_Click(object sender, EventArgs e)
+        {
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+
+            DialogResult result = folderBOutput.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBOutput.SelectedPath))
+            {
+                str_output = folderBOutput.SelectedPath;
+                MessageBox.Show(str_output + " selected as output directory");
+            }
+
+        }
+
+        private void cmdCopy_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                List<string> extensions_list = new List<string>();
+
+                //Console.WriteLine("Give input path without ending \\");
+                //str_input = Console.ReadLine();
+
+                //Console.WriteLine("Give output path without ending \\");
+                //str_output = Console.ReadLine();
+
+                // Recursive get all extensions of all files
+                foreach (string file in Directory.EnumerateFiles(str_input, "*.*", SearchOption.AllDirectories))
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    extensions_list.Add(fileInfo.Extension);
+                    counter++;
+                    Application.DoEvents();
+                }
+
+                progressBar1.Maximum = counter;
+                progressBar1.Value = 0;
+                label1.Visible = true;
+                label1.Text = "Processing file " + progressBar1.Value.ToString() + " out of "+ counter.ToString();
+                label1.Refresh();
+
+                //sort start list
+                extensions_list.Sort();
+
+                //make a list with unique items with extensions
+                List<string> d_list = new List<string>();
+
+                d_list = extensions_list.Distinct().ToList();
+
+                foreach (string extension in d_list)
+                {
+                    //create directory for every single unique extension
+                    Directory.CreateDirectory(str_output + "\\" + extension);
+                    Application.DoEvents();
+                }
+
+
+
+                // For each file in collection move it one by one in appropriate directory
+                foreach (string file_src in Directory.EnumerateFiles(str_input, "*.*", SearchOption.AllDirectories))
+                {
+                    try { 
+                        FileInfo fileInfo1 = new FileInfo(file_src);
+
+                        string file_ex_dest = fileInfo1.Extension;
+
+                        file_dest = str_output + "\\" + file_ex_dest + "\\" + fileInfo1.Name;
+
+                        if (File.Exists(file_dest))
+                        {
+                            string rnds = RandomString(3, true);
+
+                            file_dest = str_output + "\\" + file_ex_dest + "\\" + rnds + fileInfo1.Name;
+                        }
+
+                        File.Copy(file_src, file_dest, true);
+
+                        if (progressBar1.Value < progressBar1.Maximum)
+                            progressBar1.Value++; 
+
+                        label1.Text = "Processing file " + progressBar1.Value.ToString() + " out of " + counter.ToString();
+                        label1.Refresh();
+
+                        Application.DoEvents();
+                            //Console.WriteLine("Copying to " + file_dest + " OK");
+
+                        //counter++;
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.Print(ex.Message);
+                        MessageBox.Show(file_src + "\n" + "might not copied succesfully, please check.....");
+                        if (progressBar1.Value < progressBar1.Maximum)
+                            progressBar1.Value++;
+                    }
+
+                }
+                MessageBox.Show("Operation OK Categorization of " + counter.ToString() + " files OK");
+                label1.Text = "";
+                label1.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+                MessageBox.Show("Something stated as error, please check.....");
+            }
+
+        }
+    }
+}
